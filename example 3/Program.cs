463eb787d3bfee4dcbb3f1e2cd6d52afdd94242c
sergei sam <ssi1970@mail.ru>
2022-06-25 22:16:06 +0700

    задача 6

---
+++ example 3/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace HelloWorld
+{
+class Program
+{
+static void Main(string[] args)
+{
+int a;
+
+Console.WriteLine("Введите целое цисло: ");
+
+a = Convert.ToInt32(Console.ReadLine());
+if (a%2 == 0)
+{
+    Console.WriteLine("Это число - четное");
+}  
+else 
+{
+    Console.WriteLine("Это число - нечетное");
+}
+}
+}
+}
+
+
