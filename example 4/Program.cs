6fe2e40b0409a23f212168a945a6b1de0bcbc668
sergei sam <ssi1970@mail.ru>
2022-06-25 22:16:38 +0700

    задача 8

---
+++ example 4/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace HelloWorld
+{class Program
+{
+static void Main(string[] args)
+{
+int N;
+int i;
+Console.WriteLine("Введите ЛЮБОЕ целое цисло: ");
+N = Convert.ToInt32(Console.ReadLine());
+for (i=0; i<N; i++)
+{
+   if(i%2 == 0)
+  {
+    Console.Write(i + " ");
+  }
+    }
+    
+}
+}
+}
+
+
