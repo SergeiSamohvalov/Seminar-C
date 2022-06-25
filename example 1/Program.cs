1a06fab6668c500ebd009a2c67b358c05b024dde
sergei sam <ssi1970@mail.ru>
2022-06-25 22:08:44 +0700

    задача 2

---
+++ example 1/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace HelloWorld
+{
+class Program
+{
+static void Main(string[] args)
+{
+int a, b;
+
+Console.WriteLine("Введите целое цисло а: ");
+
+a = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите целое цисло b: ");
+
+b = Convert.ToInt32(Console.ReadLine());
+
+if (a > b)
+{
+   Console.WriteLine($"а - наибольшее число "); 
+}
+else
+{
+  Console.WriteLine($"b - наибольшее число ");   
+}
+}
+}
+}
